97deb88f97e068fe9ebb63f4891086eca6f7564f
AndrewMitchell25 <andrew.j.mitchell.247@gmail.com>
2024-02-20 11:23:29 +0000

    First try

---
+++ script.js
@@ -0,0 +1,31 @@
+function addDiv() {
+    let title = document.getElementById("title").value;
+    let content = document.getElementById("content").value;
+    let name = document.getElementById("name").value;
+
+
+    // Create a new div element
+    const newDiv = document.createElement('div');
+
+    // Create an h2 element
+    const newH2 = document.createElement('h2');
+    newH2.textContent = title;
+
+    const date = new Date();
+    const dateP = document.createElement('p');
+    dateP.textContent = `Posted on ${date.getMonth()}-${date.getDate()}-${date.getFullYear()} by ${name}`
+
+    // Create a p element
+    const newP = document.createElement('p');
+    newP.textContent = content;
+
+    // Append the h2 and p elements to the div
+    newDiv.appendChild(newH2);
+    newDiv.appendChild(newP);
+    newDiv.appendChild(dateP);
+
+    let blog = document.getElementById("blog");
+
+    // Append the new div to the body
+    blog.appendChild(newDiv);
+};
